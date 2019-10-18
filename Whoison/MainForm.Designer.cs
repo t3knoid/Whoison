b07f9bf124a66466f7b5dbfe1421fb67af397741
Francis Refol <frefol@strozfriedberg.com>
2019-10-18 13:43:23 -0400

    Initial commit

---
+++ Whoison/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Externals
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbUserList = new System.Windows.Forms.TextBox();
+            this.btShowUsers = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbUserList
+            // 
+            this.tbUserList.Location = new System.Drawing.Point(12, 12);
+            this.tbUserList.Multiline = true;
+            this.tbUserList.Name = "tbUserList";
+            this.tbUserList.Size = new System.Drawing.Size(260, 198);
+            this.tbUserList.TabIndex = 0;
+            // 
+            // btShowUsers
+            // 
+            this.btShowUsers.Location = new System.Drawing.Point(104, 226);
+            this.btShowUsers.Name = "btShowUsers";
+            this.btShowUsers.Size = new System.Drawing.Size(75, 23);
+            this.btShowUsers.TabIndex = 1;
+            this.btShowUsers.Text = "Show Users";
+            this.btShowUsers.UseVisualStyleBackColor = true;
+            this.btShowUsers.Click += new System.EventHandler(this.btShowUsers_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btShowUsers);
+            this.Controls.Add(this.tbUserList);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbUserList;
+        private System.Windows.Forms.Button btShowUsers;
+
+    }
+}
+
---
+++ Whoison/MainForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Externals
+{
+    public partial class MainForm : Form
+    {
+        IDictionary<string, string> Status = new Dictionary<string, string>()
+        {
+            {"WTSActive", "A user is logged on to the WinStation."},
+            {"WTSConnected", "The WinStation is connected to the client."},
+            {"WTSConnectQuery", "The WinStation is in the process of connecting to the client."},
+            {"WTSShadow", "The WinStation is shadowing another WinStation."},
+            {"WTSDisconnected", "The WinStation is active but the client is disconnected."},
+            {"WTSIdle", "The WinStation is waiting for a client to connect."},
+            {"WTSListen", "The WinStation is listening for a connection. A listener session waits for requests for new client connections. No user is logged on a listener session. A listener session cannot be reset, shadowed, or changed to a regular client session."},
+            {"WTSReset", "The WinStation is being reset."},
+            {"WTSDown",	"The WinStation is down due to an error."},
+            {"WTSInit", "The WinStation is initializing."}
+        };
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public static IntPtr OpenServer(String Name)
+        {
+            IntPtr server = Externals.WTSOpenServer(Name);
+            return server;
+        }
+        public static void CloseServer(IntPtr ServerHandle)
+        {
+            Externals.WTSCloseServer(ServerHandle);
+        }
+        private List<String> GetLoggedUsers(String ServerName)
+        {
+            IntPtr serverHandle = IntPtr.Zero;
+            List<String> resultList = new List<string>();
+            serverHandle = OpenServer(ServerName);
+
+            try
+            {
+                IntPtr SessionInfoPtr = IntPtr.Zero;
+                IntPtr userPtr = IntPtr.Zero;
+                IntPtr connectStatePtr = IntPtr.Zero;
+                Int32 sessionCount = 0;
+                Int32 retVal = Externals.WTSEnumerateSessions(serverHandle, 0, 1, ref SessionInfoPtr, ref sessionCount);
+                Int32 dataSize = Marshal.SizeOf(typeof(Externals.WTS_SESSION_INFO));
+                IntPtr currentSession = SessionInfoPtr;
+                uint bytes = 0;
+
+                if (retVal != 0)
+                {
+                    for (int i = 0; i < sessionCount; i++)
+                    {
+                        Externals.WTS_SESSION_INFO si = (Externals.WTS_SESSION_INFO)Marshal.PtrToStructure((System.IntPtr)currentSession, typeof(Externals.WTS_SESSION_INFO));
+                        currentSession += dataSize;
+
+                        if (si.pWinStationName.StartsWith("RDP-Tcp#"))
+                        {
+                            Externals.WTSQuerySessionInformation(serverHandle, si.SessionID, Externals.WTS_INFO_CLASS.WTSUserName, out userPtr, out bytes);
+                            Externals.WTSQuerySessionInformation(serverHandle, si.SessionID, Externals.WTS_INFO_CLASS.WTSConnectState, out connectStatePtr, out bytes);
+                            resultList.Add(Marshal.PtrToStringAnsi(userPtr) + " " + si.State);
+                        }
+                    }
+
+                    Externals.WTSFreeMemory(SessionInfoPtr);
+                }
+            }
+            finally
+            {
+                CloseServer(serverHandle);
+            }
+
+            return resultList;
+        }
+
+        private void btShowUsers_Click(object sender, EventArgs e)
+        {
+            List<String> users = GetLoggedUsers("localhost");
+            tbUserList.Clear();
+            tbUserList.Text = String.Join(Environment.NewLine, users);
+        }
+    }
+}
